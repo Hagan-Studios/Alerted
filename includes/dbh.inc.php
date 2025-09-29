fcde137a12327d8d94d288ed311c8e78901d8bdc
Wasabi Man <103323500+Hagan-Studios@users.noreply.github.com>
2025-09-29 16:18:45 +0530

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,1 +1,13 @@
+<?php
 
+$dsn = 'mysql:host=localhost;dbname=db';
+$dbusername = 'root';
+$dbpassword = '';
+
+try {
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo '<h1 style="font-family: monospace; font-size: 2vw; text-align: center;">Database connection failed: ' . $e->getMessage() . '</h1>';
+    exit();
+}
